7cfe0414f7085084425d744e90e86e6504af3a9d
Ahmad Moussawi <aymoussawi@gmail.com>
2019-03-07 22:25:45 +0200

    add multiple views

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,34 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function loadView(view) {
+    return $.get('views/' + view + '.html');
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function loadViews(views) {
+    return Promise.all(views.map(function (view) {
+        return loadView(view);
+    })).then(function (viewsHtml) {
+        viewsHtml.forEach(function (html, i) {
+            $('page[name=' + views[i] + ']').append(html);
+        });
+    });
+}
 
-        console.log('Received Event: ' + id);
-    }
-};
+function go(view)
+{
+    $('page').removeClass('active');
+    $('page[name=' + view + ']').addClass('active');
+}
+
+// Define a placeholder where that holds all partial views
+var $placeholder = $('placeholder');
+
+var views = $('placeholder page').map(function (i, page) {
+    return $(page).attr('name')
+}).toArray();
+
+loadViews(views).then(function () {
+
+    // here all view are ready
+
+    // alert('Views are loaded');
+
+})
